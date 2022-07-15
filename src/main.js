4c44aef592f2014a23d94b9d0bb0a188bacbfa64
quangnd <quangnd.nta@gmail.com>
2022-07-15 15:39:28 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ const SqlConnection = require('./configs/sql_connection');
 
 const app = require('./app');
 
-function main() {
-    var sqlConnection = SqlConnection();
-    sqlConnection.conn.connect((error) => {
-        if (error) throw error;
-        console.log("Connected");
-        app.listen(3000);
-    });
-}
-main();
+SqlConnection().conn.connect((error) => {
+    if (error) throw error;
+    console.log("Connected");
+    app.listen(3000);
+});
