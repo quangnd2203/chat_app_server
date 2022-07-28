dfc9b6affbce0c818fe3c4a3b964ea184ee2406c
quangnd <quangnd.nta@gmail.com>
2022-07-28 11:28:54 +0700

    no message

--- chat_app_db.sql
+++ chat_app_db.sql
@@ -30,15 +30,17 @@ DELIMITER $$
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `systemGetStatus` (IN `pMessage` VARCHAR(50), IN `pStatus` INT)   SELECT `pMessage` AS message, `pStatus` AS 'status'$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `userAuthorize` (IN `pEmail` VARCHAR(50), IN `pAccessToken` VARCHAR(500))   SELECT * FROM `User` WHERE `email` = pEmail AND `accessToken` = pAccessToken$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `userAuthorize` (IN `pEmail` VARCHAR(50), IN `pAccessToken` VARCHAR(500))   BEGIN
+SELECT * FROM `User` WHERE `email` = pEmail AND `accessToken` = pAccessToken;
+END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `userGetAll` ()   SELECT * FROM User$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `userGetAll` (IN `pUid` TEXT)   SELECT * FROM User WHERE `uid` != pUid$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `userLogin` (IN `pEmail` VARCHAR(50), IN `pPassword` VARCHAR(200), IN `pAccountType` ENUM('normal','facebook','google'), IN `pAccessToken` VARCHAR(500), IN `pFcmToken` VARCHAR(200))   BEGIN
 IF(`pAccountType` = 'normal') THEN
 	BEGIN
-    	SELECT * FROM `User` WHERE `email` = pEmail AND `password` = pPassword AND `accountType` = pAccountType;
         UPDATE `User` SET `accessToken` = pAccessToken, `fcmToken` = pFcmToken WHERE `email` = pEmail AND `password` = pPassword AND `accountType` = pAccountType;
+        SELECT * FROM `User` WHERE `email` = pEmail AND `password` = pPassword AND `accountType` = pAccountType;
     END;
 ELSE
 	BEGIN
@@ -123,11 +125,8 @@ CREATE TABLE `User` (
 --
 
 INSERT INTO `User` (`id`, `uid`, `name`, `email`, `accountType`, `password`, `avatar`, `background`, `accessToken`, `fcmToken`, `created_at`, `updated_at`) VALUES
-(25, 'uid-1ba6d01c-0cb5-11ed-876f-c6ef0857e0cf', 'quang nguyen', '110886710069326929110', 'google', NULL, NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiMTEwODg2NzEwMDY5MzI2OTI5MTEwIiwiaWF0IjoxNjU4ODIxODg0LCJleHAiOjE2OTAzNTc4ODR9.ojASS1m_LJlBUCjubO4XeEMtUcHXHpZMF6iwLZRGsrXGjBzRISMlC9uylMxs8KGFbD6Yc48jiqXm1IOK8Eb5bw', NULL, '2022-07-26 07:32:31', '2022-07-26 14:51:24'),
-(4, 'uid-25016d36-040c-11ed-b42c-c6ef0857e0cf', 'Nguyễn Đăng Quang', 'quangnd.nta@gmail.com', 'normal', '32ff5fea7d6c46c0590a4f3bbc3293f54025ab332a59d5d3a3c8271920b857470f3c71b90c3b4f70cba88c0d3a2a5ce8d065332b3e9e539a5f7e7ec0fcfaaf3f', NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicXVhbmduZC5udGFAZ21haWwuY29tIiwiaWF0IjoxNjU4OTA5ODA5LCJleHAiOjE2OTA0NDU4MDl9.xQR2n0y-0oJLxwNzn7n3ysG7-XyUH9EMxFhb4RDY5CCgQ-1F8qwrqV71WpLbZVYsN61q2QKDTkRjXfD-hdgqaQ', 'sdsdsd', '2022-07-15 07:02:52', '2022-07-27 15:16:49'),
-(26, 'uid-375100e6-0cc7-11ed-876f-c6ef0857e0cf', 'Nguyễn Đăng Quang', '413376224136632', 'facebook', NULL, NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiNDEzMzc2MjI0MTM2NjMyIiwiaWF0IjoxNjU4ODI4ODczLCJleHAiOjE2OTAzNjQ4NzN9.g4IN5Ssd0Mt1TyQtgFxD7bNsghSU-w4TUPoVv5Uk9Zl5wElSBMaMkU6RlvrliJZawo6P0c7VeN5WA3ZNdE6lCg', NULL, '2022-07-26 09:42:08', '2022-07-26 16:47:53'),
-(27, 'uid-37ed85fa-0cc8-11ed-876f-c6ef0857e0cf', 'Kim Trâm Lê Thị', '111471591272174862520', 'google', NULL, NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiMTExNDcxNTkxMjcyMTc0ODYyNTIwIiwiaWF0IjoxNjU4ODI4OTU4LCJleHAiOjE2OTAzNjQ5NTh9.--AAMhkJXkPMKr0-W3H5-V4hxD39-iUgyBG3P7JR1bv6UoEqQLnWm-r5R7SSZsjOtbxqUAF3uE2Ps9y0xmmdMQ', NULL, '2022-07-26 09:49:18', '2022-07-26 16:49:18'),
-(24, 'uid-3cac1894-0bfc-11ed-aca7-c6ef0857e0cf', 'Kim Tram Le', 'kimtramle@gmail.com', 'normal', 'Aa22032001!', NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoia2ltdHJhbWxlQGdtYWlsLmNvbSIsImlhdCI6MTY1ODc0MTM0OSwiZXhwIjoxNjkwMjc3MzQ5fQ.EQ8pF0oL2861q0WhGw1LLsZ12Wb-0v3xYYEwHQAe_CE6s7_BMNx7WRChwn6ZJ8Q7vOYUGGwFhgM_kZbi4giMJg', 'test', '2022-07-25 09:29:09', '2022-07-25 16:29:09');
+(33, 'uid-89172ad8-0e28-11ed-b1fe-c6ef0857e0cf', 'Nguyễn Đăng Quang', 'quangnd.nta@gmail.com', 'normal', '32ff5fea7d6c46c0590a4f3bbc3293f54025ab332a59d5d3a3c8271920b857470f3c71b90c3b4f70cba88c0d3a2a5ce8d065332b3e9e539a5f7e7ec0fcfaaf3f', NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicXVhbmduZC5udGFAZ21haWwuY29tIiwiaWF0IjoxNjU4OTgwMjc3LCJleHAiOjE2OTA1MTYyNzd9.i19ZDp6kmqZbEeDnnMarGoHogNOjE6sNDNnMzF46rtKV1rB4FGXgFIRfI78yXJR9-zrE6z176jY-aIXeo2VLJA', 'cypWnjRyRaGrvxldi17k1X:APA91bFZ8g_kG3wStXZr-Nf72AqrAEPHDdUqHpuEcTnTrymwe5miTWYy8cGZbhgU11wyWiGo5IORhI0mfa5f7v1izxvqjpvP5_mmoj5XIdVVKOrZsWLhaArtIeoBG6tzlOy-QF39TpG1', '2022-07-28 03:51:17', '2022-07-28 10:51:17'),
+(31, 'uid-c5122900-0e20-11ed-b1fe-c6ef0857e0cf', 'Kim Trâm Lê Thị', '111471591272174862520', 'google', NULL, NULL, NULL, 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiMTExNDcxNTkxMjcyMTc0ODYyNTIwIiwiaWF0IjoxNjU4OTc2OTQyLCJleHAiOjE2OTA1MTI5NDJ9.VOi541BAy3YHmlFJk26djWh2ADWPBtIBFlIrBWXuSjURovJE16MjOWJqb1Jb3iaSO34VaAeMXFW1nflLD12DTg', 'cypWnjRyRaGrvxldi17k1X:APA91bFZ8g_kG3wStXZr-Nf72AqrAEPHDdUqHpuEcTnTrymwe5miTWYy8cGZbhgU11wyWiGo5IORhI0mfa5f7v1izxvqjpvP5_mmoj5XIdVVKOrZsWLhaArtIeoBG6tzlOy-QF39TpG1', '2022-07-28 02:55:42', '2022-07-28 09:55:42');
 
 -- --------------------------------------------------------
 
@@ -196,7 +195,7 @@ ALTER TABLE `Message`
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
